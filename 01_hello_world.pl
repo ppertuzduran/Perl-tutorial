de8b01376040d85f86f9f8653a8933e8fa5d7b82
Pablo Andres Pertuz Duran <71804912+ppertuzduran@users.noreply.github.com>
2023-05-04 15:10:14 -0500

    Add files via upload

---
+++ 01_hello_world.pl
@@ -0,0 +1,8 @@
+#!/bin/perl (perl route)
+
+#Autor: Pablo Pertuz
+#Date: May 4th 2023
+#Contact: pablo@ayte.co
+#Description: screen message
+
+print "Hi perl pentesters!!!\n"; #Print an on-screen message.
